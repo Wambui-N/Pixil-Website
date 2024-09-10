580ee08b7e88d14658db79e0f603acd85f27f80a
Wambui Ndungu <wambuin18@gmail.com>
2024-09-10 17:03:27 +0300

    Install fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,31 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      brule: ["Brule", "sans-serif"],
+      satoshi: ["Satoshi_Variable", "sans-serif"],
+    },
+    container: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        transparent: "transparent",
+        white: "#FBFCF8",
+        black: "#212121",
+        grey: "#666666",
+        orange: "#DE4A24",
+        iron: "#DEEBD3"
       },
     },
   },

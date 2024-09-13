a6f5387c966acf248e1ba2fda7965aad5e082c09
Wambui Ndungu <wambuin18@gmail.com>
2024-09-13 15:27:04 +0300

    Create Footer

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -2,10 +2,12 @@ import Navigation from "./Navigation";
 import Hero from "./Hero"
 import Benefits from "./Benefits";
 import Quiz from "./Quiz";
+import Footer from "./Footer";
 
 export {
     Navigation,
     Hero,
     Benefits,
     Quiz,
+    Footer,
 }
